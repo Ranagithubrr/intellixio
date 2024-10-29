496020f761b99196df6034e5d3c60978d83b574e
ranagithubrr <ranarr.dev@gmail.com>
2024-10-30 01:09:17 +0600

    Fix: typescript issue fixed

--- src/views/userAgent/userAgent.tsx
+++ src/views/userAgent/userAgent.tsx
@@ -1,24 +1,18 @@
 "use client";
-
 import { BackToHome } from "@/components/backToHome/backToHome";
-import { useUserAgentContext } from "@/components/providers/userAgentProvider";
+interface UserAgentProps {
+  userAgent: string;
+}
 
-export const UserAgent = () => {
-  const { userAgent } = useUserAgentContext();
-
+export const UserAgent: React.FC<UserAgentProps> = ({ userAgent }) => {
   return (
     <div>
       <BackToHome />
 
-      {userAgent && (
-        <div className="flex font-mono font-semibold text-sm">
-          <div className="border p-2">UserAgent</div>
-
-          <div className="border p-2">{userAgent}</div>
-        </div>
-      )}
-
-      {!userAgent && <div>No user agent</div>}
+      <div className="flex font-mono font-semibold text-sm">
+        <div className="border p-2">UserAgent</div>
+        <div className="border p-2">{userAgent}</div>
+      </div>
     </div>
   );
 };
